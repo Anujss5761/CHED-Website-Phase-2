f2cad5bee4fbda83abf33f9c9d713feccb7835dc
Anuj Kashyap <anujs5761@gmail.com>
2024-04-19 04:52:15 +0530

    mm

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-const counters = document.querySelectorAll(".fac-p-c1");
-const container = document.querySelector("fac-p-n");
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
 
-let activated = false;
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
 
-window.addEventListener("scroll", ()=>{
-    if (pageXOffset > container.offsetTop - container.offsetHeight - 200
-        && activated === false) {
-        counters.forEach(counter =>{
-            counter.innerText = 0;
-            let count = 0;
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
 
-            function updateCount() {
-                const target = parseInt(counter.dataset.count);
-                if (count < target) {
-                    count++;
-                    counter.innerText = count;
-                    setTimeout(updateCount, 10);
-                } else{
-                    counter.innerText = target;
-                }
-            }
-            updateCount();
-            activated = true;
-        });
-    }else if(
-        pageXOffset < container.offsetTop - container.offsetHeight - 500
-        || pageXOffset === 0
-        && activated
-    ) {
-        counters.forEach(counter =>{
-            counter.innerText = 0;
-        });
-        activated = false;
-    }
-});
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
 
