fdabd89c06626b6c05324f0a58d1e259b1c2a6ac
Anuj Kashyap <anujs5761@gmail.com>
2024-04-20 00:44:20 +0530

    add

--- script.js
+++ script.js
@@ -38,3 +38,25 @@ window.onresize = function(event) {
     reloadSlider();
 };
 
+
+//Scroll Animation
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for( i =0; i < reveals.length; i++){
+
+       var windowheight = window.innerHeight;
+       var revealtop = reveals[i].getBoundingClientRect().top;
+       var revealpoint = 150;
+
+       if (revealtop < windowheight - revealpoint) {
+          reveals[i].classList.add('active');
+       } else{
+        reveals[i].classList.remove('active');
+       }
+    }
+}
+
